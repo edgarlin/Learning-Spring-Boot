06ce7f0fd818d7fc720939bf83408c9d8f8d15d1
edgar <edgar>
2018-06-09 18:33:26 +0800

    profiles

--- src/main/java/com/demo/runners/CommandLine.java
+++ src/main/java/com/demo/runners/CommandLine.java
@@ -3,9 +3,11 @@ package com.demo.runners;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("prod")
 public class CommandLine implements CommandLineRunner {
 	private static final Logger LOGGER = LoggerFactory.getLogger(CommandLine.class);
 
