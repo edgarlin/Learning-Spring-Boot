6785e7d71a29f4890ed6259e0f92d944a55a44cd
edgar <edgar>
2018-06-09 18:21:01 +0800

    banner

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -1,5 +1,6 @@
 package com.demo;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+		SpringApplication app = new SpringApplication(App.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
 	}
 }
